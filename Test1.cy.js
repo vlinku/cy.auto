bd7e26a785326f047286e1d5a224449456d3441a
vlinku <vilius.linkunaitis@gmail.com>
2022-07-23 12:02:11 +0300

    Upload project to github

---
+++ Test1.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+describe('My First Test', function () {
+    it('My first test case.', function () {
+
+        cy.visit("https://www.g2ocean.com/");
+
+    })
+})
