33786f27f9281d518b35e16c92323a2a51ef5503
vlinku <vilius.linkunaitis@gmail.com>
2022-08-10 21:39:53 +0300

    Test Upload to Branch Lecture3Update

--- Lecture3/Searchbox.cy.js
+++ Lecture3/Searchbox.cy.js
@@ -1,1 +1,15 @@
-describe('Searchbox upload', () => { })
+describe('Searchbox test', () => {
+
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+
+    it('type text in the search box and press enter', () => {
+        cy.get('#searchTerm').type('money{enter}')
+    })
+
+    it('show search results in the page', () => {
+        cy.get('h2').contains('Search Results:')
+    })
+
+})
